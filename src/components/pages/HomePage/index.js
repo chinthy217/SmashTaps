f7bcc65bc5670a2536b5022d003baae229f9567b
Chinthaka Gunasinghe <chinthaka@jkcsworld.com>
2020-06-12 16:59:05 +0530

    simple_change

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,9 +1,13 @@
 // https://github.com/diegohaz/arc/wiki/Atomic-Design
 import React from 'react'
+import { Heading } from 'components'
 
 const HomePage = () => {
   return (
-    <div>Hello World</div>
+    <>
+      <Heading level={3}>Navigate to /sample-page</Heading>
+      <p>Make sure you have installed CORS extention on your browser</p>
+    </>
   )
 }
 
